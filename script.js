f6b717cd7d5e99e576b653c2ee4b6b885359f753
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-09-11 14:57:52 -0400

    adding arrays in

--- script.js
+++ script.js
@@ -1,41 +1,22 @@
 
-var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-var spChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+var spChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-var cap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var cap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-var lwr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var lwr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var all = [num, spChar, cap, lwr]
+function generatePassword() {
 
-
-passwordLength = prompt("How long would you like your password to be? must be between 8-129")
-console.log(passwordLength)
-
-finalPass 
-
-for (let i = 0; i < array.length; i++) {
-   
-  
 }
-
-
-
-
-
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(password);
-  var passwordText = document.querySelector(password);
-
-  passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
