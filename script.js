3a1355ea063a310f0c90f69d2a7f9386f6339d2e
Brandon Green <Brandon.Green@knights.ucf.edu>
2020-09-11 14:59:27 -0400

    added if else statements for the value of password length and added user confirm for spc characters ,etc.

--- script.js
+++ script.js
@@ -11,6 +11,22 @@ function generatePassword() {
   passwordLength = prompt("How long would you like your password to be? must be between 8-129");
 
   console.log(passwordLength);
+  
+  if (passwordLength <= 7) {
+    alert("must be at least 8 characters");
+  }
+  else if (passwordLength >= 130) {
+    alert("must be 129 characters or less");
+  }
+  else {
+
+    passwordSp = confirm("would you like special characters");
+    // console.log(passwordSp)
+    passwordCap = confirm("would you like capital letters?");
+    passwordLwr = confirm("would you like lower case letters?");
+    passwordNum = confirm("would you like numbers?");
+
+  }
 
 
 }
